d4641ae1278808e7d351658b0a45c9e905011795
R <tamilsup@amazon.com>
2026-03-10 18:56:47 +0530

    Fix static path and root route

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve your client folder (adjust path if needed)
-app.use(express.static("../client"));
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId) => {
         socket.join(roomId);
         socket.to(roomId).emit("user-connected", socket.id);
 
-        socket.on("signal", (data) => {
+        socket.on("signal", data => {
             io.to(data.to).emit("signal", {
                 from: socket.id,
                 signal: data.signal
@@ -27,7 +31,6 @@ io.on("connection", (socket) => {
     });
 });
 
-// Use Railway's assigned port or fallback to 3000 locally
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
